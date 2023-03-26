007e5b5a01459676543e82c91e32ff52f2ace97b
auttmme <ichautm14@gmail.com>
2023-03-26 18:04:40 +0700

    feat(filter): create filter component

--- src/components/home/Filter.jsx
+++ src/components/home/Filter.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
+import {
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	Button,
+	Select,
+} from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 
 export default function Filter() {
 	return (
-		<Menu>
-			<MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-				All
-			</MenuButton>
-			<MenuList>
-				<MenuItem>Most Emailed</MenuItem>
-				<MenuItem>Most Shared</MenuItem>
-				<MenuItem>Most Viewed</MenuItem>
-			</MenuList>
-		</Menu>
+		<Select placeholder="All" width={["45%", "10%"]}>
+			<option>Most Emailed</option>
+			<option>Most Shared</option>
+			<option>Most Viewed</option>
+		</Select>
 	);
 }
