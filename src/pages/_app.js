50dc1f12a618e28a32b3448728b9df778652acbc
auttmme <ichautm14@gmail.com>
2023-03-26 19:13:02 +0700

    feat(_app): create theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,21 @@
 import "@/styles/globals.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+	colors: {
+		brand: {
+			300: "#1999CF",
+			500: "#2F74BA",
+			800: "#20334D",
+		},
+	},
+});
 
 export default function App({ Component, pageProps }) {
 	return (
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<Component {...pageProps} />
 		</ChakraProvider>
 	);
