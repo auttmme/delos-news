3df392960db6bc0938d4a52489a1e2b3ba5b6337
auttmme <ichautm14@gmail.com>
2023-03-26 19:13:15 +0700

    feat(detail): create detail article page

---
+++ src/pages/detail.js
@@ -0,0 +1,19 @@
+import BuyArticleCard from "@/components/article/BuyArticleCard";
+import Layout from "@/components/common/Layout";
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import ArticleDetail from "src/components/article/ArticleDetail";
+
+export default function detail() {
+	return (
+		<Layout>
+			<Flex
+				justifyContent={"space-between"}
+				flexDirection={["column", "column", "column", "row"]}
+			>
+				<ArticleDetail />
+				<BuyArticleCard />
+			</Flex>
+		</Layout>
+	);
+}
