754be436aa84b10c4f3464d6dae67bb9ddd56778
auttmme <ichautm14@gmail.com>
2023-03-26 19:23:16 +0700

    feat(luckyDraw): create lucky draw page

---
+++ src/pages/luckyDraw.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/common/Layout";
+import { Text } from "@chakra-ui/react";
+import React from "react";
+
+export default function luckyDraw() {
+	return (
+		<Layout>
+			<Text>Lucky Draw</Text>
+		</Layout>
+	);
+}
