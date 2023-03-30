fc20a1970282a8685aac33671e54d1028aba40e0
auttmme <ichautm14@gmail.com>
2023-03-30 19:07:17 +0700

    breaking-change(filter): create filter component with chakra

--- src/components/home/Filter.jsx
+++ src/components/home/Filter.jsx
@@ -1,20 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	Menu,
 	MenuButton,
 	MenuList,
-	MenuItem,
 	Button,
-	Select,
+	Stack,
+	Checkbox,
+	CheckboxGroup,
 } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 
-export default function Filter() {
+export default function Filter({ onCategory }) {
+	const [checkedItems, setCheckedItems] = useState([true, true, true]);
+
+	const allChecked = checkedItems.every(Boolean);
+	const isIndeterminate = checkedItems.some(Boolean) && !allChecked;
+
+	const [category, setCategory] = useState([]);
+
+	const sendCategory = () => {
+		onCategory();
+	};
+
+	console.log("value", category);
+
 	return (
-		<Select placeholder="All" width={["45%", "10%"]}>
-			<option>Most Emailed</option>
-			<option>Most Shared</option>
-			<option>Most Viewed</option>
-		</Select>
+		<Menu>
+			<MenuButton marginBottom={5} as={Button} rightIcon={<ChevronDownIcon />}>
+				Filter
+			</MenuButton>
+			<MenuList padding={4}>
+				<CheckboxGroup colorScheme={"facebook"}>
+					<Stack>
+						<Checkbox
+							defaultChecked
+							isChecked={allChecked}
+							value="all"
+							isIndeterminate={isIndeterminate}
+							onChange={(e) => {
+								setCheckedItems([
+									e.target.checked,
+									e.target.checked,
+									e.target.checked,
+								]);
+								setCategory(e.target.value);
+							}}
+						>
+							All
+						</Checkbox>
+						<Checkbox
+							isChecked={checkedItems[0]}
+							onChange={(e) =>
+								setCheckedItems([
+									e.target.checked,
+									checkedItems[1],
+									checkedItems[2],
+								])
+							}
+						>
+							Most Viewed
+						</Checkbox>
+						<Checkbox
+							isChecked={checkedItems[1]}
+							onChange={(e) =>
+								setCheckedItems([
+									checkedItems[0],
+									e.target.checked,
+									checkedItems[2],
+								])
+							}
+						>
+							Most Shared
+						</Checkbox>
+						<Checkbox
+							isChecked={checkedItems[2]}
+							onChange={(e) =>
+								setCheckedItems([
+									checkedItems[0],
+									checkedItems[1],
+									e.target.checked,
+								])
+							}
+						>
+							Most Emailed
+						</Checkbox>
+					</Stack>
+				</CheckboxGroup>
+			</MenuList>
+		</Menu>
 	);
 }
